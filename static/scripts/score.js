1167274ed52de87e8c9b8c558eb6d3b9095c4490
Thinh <thinh1198@hotmail.com>
2024-01-15 22:05:55 +0000

    code to clear highscores

--- static/scripts/score.js
+++ static/scripts/score.js
@@ -1,8 +1,19 @@
 var scoreTableEl = document.querySelector('#highscores');
+var clearEl = document.querySelector('#clear');
 
-for (var i = 0; i < localStorage.length; i++) {
-    var li = document.createElement('li');
-    li.textContent = localStorage.getItem(i+1)
-    scoreTableEl.append(li)
+function loadStorage() {
+    for (var i = 0; i < localStorage.length; i++) {
+        var li = document.createElement('li');
+        li.textContent = localStorage.getItem(i+1)
+        scoreTableEl.appendChild(li)
+    }
 }
 
+clearEl.addEventListener('click', () => {
+    for (var i = 0; i < localStorage.length; i++) {
+        scoreTableEl.removeChild(scoreTableEl.firstChild);
+    }
+    localStorage.clear();
+})
+
+loadStorage();
